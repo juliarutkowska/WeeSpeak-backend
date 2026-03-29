0d4094c84d124fea43d52087832f729a3dcc53c9
juliarutkowska <julia.rutkowska189@gmail.com>
2026-03-29 20:43:23 +0200

    Add support for optional local configuration via appsettings.Local.json

--- WeeSpeak/Program.cs
+++ WeeSpeak/Program.cs
@@ -6,6 +6,8 @@ const string CorsPolicy = "AllowFrontend";
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
